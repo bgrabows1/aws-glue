10400933e8e4415f52fc58180d4089968d530a71
bgrabows1 <bgrabowski.bartek@gmail.com>
2019-06-12 14:12:08 +0200

    EKS work in progress.

--- terraform/eks/cluster.tf
+++ terraform/eks/cluster.tf
@@ -1,80 +1,74 @@
-resource "aws_iam_role" "eks-cluster" {
-  name = "terraform-eks-cluster"
+resource "aws_eks_cluster" "this" {
+  name                      = "${var.cluster_name}"
+  enabled_cluster_log_types = "${var.cluster_enabled_log_types}"
+  role_arn                  = "${local.cluster_iam_role_arn}"
+  version                   = "${var.cluster_version}"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
+  vpc_config {
+    security_group_ids      = ["${local.cluster_security_group_id}"]
+    subnet_ids              = ["${var.subnets}"]
+    endpoint_private_access = "${var.cluster_endpoint_private_access}"
+    endpoint_public_access  = "${var.cluster_endpoint_public_access}"
+  }
+
+  timeouts {
+    create = "${var.cluster_create_timeout}"
+    delete = "${var.cluster_delete_timeout}"
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy",
   ]
 }
-POLICY
+
+resource "aws_security_group" "cluster" {
+  count       = "${var.cluster_create_security_group ? 1 : 0}"
+  name_prefix = "${var.cluster_name}"
+  description = "EKS cluster security group."
+  vpc_id      = "${var.vpc_id}"
+  tags        = "${merge(var.tags, map("Name", "${var.cluster_name}-eks_cluster_sg"))}"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.eks-cluster.name}"
+resource "aws_security_group_rule" "cluster_egress_internet" {
+  count             = "${var.cluster_create_security_group ? 1 : 0}"
+  description       = "Allow cluster egress access to the Internet."
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.cluster.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  type              = "egress"
 }
 
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.eks-cluster.name}"
-}
-
-resource "aws_security_group" "eks-cluster" {
-  name        = "terraform-eks-cluster"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = "${aws_vpc.eks-vpc.id}"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terraform-eks-cluster"
-  }
-}
-
-resource "aws_security_group_rule" "eks-cluster-ingress-node-https" {
-  description              = "Allow pods to communicate with the cluster API Server"
-  from_port                = 443
+resource "aws_security_group_rule" "cluster_https_worker_ingress" {
+  count                    = "${var.cluster_create_security_group ? 1 : 0}"
+  description              = "Allow pods to communicate with the EKS cluster API."
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.eks-cluster.id}"
-  source_security_group_id = "${aws_security_group.eks-node.id}"
+  security_group_id        = "${aws_security_group.cluster.id}"
+  source_security_group_id = "${local.worker_security_group_id}"
+  from_port                = 443
   to_port                  = 443
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  cidr_blocks       = ["${local.workstation-external-cidr}"]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.eks-cluster.id}"
-  to_port           = 443
-  type              = "ingress"
+resource "aws_iam_role" "cluster" {
+  count                 = "${var.manage_cluster_iam_resources ? 1 : 0}"
+  name_prefix           = "${var.cluster_name}"
+  assume_role_policy    = "${data.aws_iam_policy_document.cluster_assume_role_policy.json}"
+  permissions_boundary  = "${var.permissions_boundary}"
+  path                  = "${var.iam_path}"
+  force_detach_policies = true
 }
 
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = "${var.cluster-name}"
-  role_arn = "${aws_iam_role.eks-cluster.arn}"
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  count      = "${var.manage_cluster_iam_resources ? 1 : 0}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.cluster.name}"
+}
 
-  vpc_config {
-    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.eks-subnet.*.id}"]
-  }
-
-  depends_on = [
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
-  ]
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
+  count      = "${var.manage_cluster_iam_resources ? 1 : 0}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.cluster.name}"
 }
